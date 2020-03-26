3f8665bd8ae55f3e702ee0ab9001d3bed7fb2f64
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-26 00:32:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,12 @@ def oxford_comma(arr)
   str = ""
   
   arr.each { |el| 
-    if arr.size == 1 {str << el}
-    elsif arr.size ==2{arr.to_s}
-  end
-  }
+    if arr.size == 1
+      str << el
+    elsif arr.size ==2
+    arr.to_s("and")
+    end
+        }
     
   str
 end
