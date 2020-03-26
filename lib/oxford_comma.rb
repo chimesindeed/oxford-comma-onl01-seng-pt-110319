31487c97a819dfc562f6c273469980cbfe9855ce
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-26 00:49:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 require 'pry'
 def oxford_comma(array)
-  if 2 < array.length
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
     array[-1].insert(0, "and ")
   end
   array.join(", ")    
